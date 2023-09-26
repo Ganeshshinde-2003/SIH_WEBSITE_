21d9b809cf3597afb30c5800b0df4ce856ace31b
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-27 02:08:24 +0530

    created thre review page

--- src/components/PdfReview.jsx
+++ src/components/PdfReview.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import book1 from "../assets/book1.jpg";
 
 const PdfReview = () => {
   return (
-    <div>
-      <Link to="http://localhost:3001/custom-image-stamp">Edit</Link>
+    <div className="pdf-container">
+      <p className="pdf-heading">Review Book</p>
+      <div className="pdf-content">
+        <div className="reviewer-details">
+          <p>
+            Name <br /> <span>Anuradha Goyal</span>
+          </p>
+          <p>
+            Qualification <br /> <span>MBBS @Victoria</span>
+          </p>
+
+          <p>
+            Affiliation <br /> <span>No 1 author of 2002. by AIIMS Delhi</span>
+          </p>
+        </div>
+        <div>
+          <img src={book1} alt="alt" />
+        </div>
+      </div>
     </div>
   );
 };
